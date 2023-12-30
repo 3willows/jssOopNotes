cfab1f7b7570abf45af5c3e65d046c9b0719747a
Jasper Wong <wlcjasper@gmail.com>
2023-12-30 16:31:44 +0800

    use class not prototype

--- colorsTranslator.js
+++ colorsTranslator.js
@@ -6,7 +6,7 @@ const rgb = (r, g, b) => {
 }
 
 // console.log(hex(0, 51, 255))
-// This is the "factory" pattern (???)
+// OLDEST WAY: This is the "factory" pattern (???)
 
 function makeColor (r, g, b) {
   const color = {}
@@ -25,13 +25,14 @@ function makeColor (r, g, b) {
 }
 
 // the problem is that fucntions are unnecessarily copied
-
+// MIDDLE WAY: Prototype
 function Color (r, g, b) {
   this.r = r
   this.g = g
   this.b = b
 }
 
+// If you use arrow functions, this will point to the whole window
 Color.prototype.rgb = function () {
   const { r, g, b } = this
   return `rgb(${r},${g},${b})`
@@ -47,13 +48,47 @@ Color.prototype.rgba = function (a = 1.0) {
   return `rgba(${r},${g},${b},${a})`
 }
 
-// If you don't use a new prorperty, this will point towards the whole Window.
+// LATEST WAY: Class
+// Just better packaged
 
-const firstColor = new makeColor(0, 51, 255)
+class example {
+  constructor (r, g, b) {
+    console.log('in constructor!')
+    console.log(r, g, b)
+  }
+}
+
+// when you create a new instance, constructor runs immediately
+// const c1 = new example(222, 2, 22)
+
+class classyColor {
+  constructor (r, g, b, names) {
+    this.r = r
+    this.g = g
+    this.b = b
+    this.name = name
+  }
+  innerRgb () {
+    const { r, g, b } = this
+    return `${r},${g},${b}`
+  }
+  rgb () {
+    return `rgb(${this.innerRgb()})`
+  }
+  rgba (a = 1) {
+    return `rgba(${this.innerRgb()},${a})`
+  }
+  hex () {
+    const { r, g, b } = this
+    return `#` + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+  }
+}
+
+const c1 = new classyColor(170, 80, 105, 'classy purple')
 
 // DOM
 
-const colorTitle = document.querySelector('#colorTitle')
+const colorTitles = document.querySelectorAll('.colorTitle')
 const form = document.querySelector('form')
 
 form.addEventListener('change', function (e) {
@@ -64,10 +99,16 @@ form.addEventListener('change', function (e) {
   const b = formData.get('b') // Retrieves the 'b' value from form data
 
   console.log(`Red: ${r}, Green: ${g}, Blue: ${b}`)
-  colorTitle.style.color = `rgb(${r}, ${g}, ${b})`
+  const rgbColor = `rgb(${r}, ${g}, ${b})`;
 
-  // const colorObject = makeColor(r, g, b)
-  const colorObject = new Color(r, g, b)
+  // Iterate through each .colorTitle element and apply the style
+  colorTitles.forEach(colorTitle => {
+    colorTitle.style.color = rgbColor
+  })
+
+  // const k = makeColor(r, g, b)
+  // const colorObject = new Color(r, g, b)
+  const colorObject = new classyColor(r, g, b)
 
   const rgbValues = document.querySelector('#rgbValues')
   const hexValues = document.querySelector('#hexValues')
